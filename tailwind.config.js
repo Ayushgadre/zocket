62e436eef52afabde7277ee307a8268656377f73
ayushgadre <ayushgadre786@gmail.com0>
2024-05-16 22:57:17 +0530

    Updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
-/** @type {import('tailwindcss').Config} */
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+
+// tailwind.config.js
 module.exports = {
-  content: [],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './public/index.html',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
